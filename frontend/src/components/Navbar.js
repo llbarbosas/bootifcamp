01fbddd58505b83acff3106b594fcad29f585225
Jorge Dullius <jorge.dullius.19@gmail.com>
2019-08-01 09:10:33 -0400

    Implementando responsividade na navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,3 +1,4 @@
+import MediaQuery from "react-responsive";
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
@@ -9,10 +10,10 @@ const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1
   },
-  appBar:{
-    backgroundColor: '#01579B'
+  appBar: {
+    backgroundColor: "#01579B"
   },
-  firstButton:{
+  firstButton: {
     marginRight: theme.spacing(4),
     color: "#60C83C"
   },
@@ -23,10 +24,8 @@ const useStyles = makeStyles(theme => ({
     flexGrow: 1
   }
 }));
-
-export default function ButtonAppBar() {
+function AppBarDesktop() {
   const classes = useStyles();
-
   return (
     <div className={classes.root}>
       <AppBar position="static" className={classes.appBar}>
@@ -34,12 +33,29 @@ export default function ButtonAppBar() {
           <Typography variant="h6" className={classes.title}>
             Estudaê
           </Typography>
-          <Button color="inherit" className={classes.menuButton}>Home</Button>
-          <Button color="inherit" className={classes.menuButton}>Consultar</Button>
-          <Button color="inherit" className={classes.menuButton}>Praticar</Button>
-          <Button color="inherit" className={classes.firstButton}>Sou professor</Button>
+          <Button color="inherit" className={classes.menuButton}>
+            Home
+          </Button>
+          <Button color="inherit" className={classes.menuButton}>
+            Consultar
+          </Button>
+          <Button color="inherit" className={classes.menuButton}>
+            Praticar
+          </Button>
+          <Button color="inherit" className={classes.firstButton}>
+            Sou professor
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
   );
 }
+
+const AppBar = () => (
+  <div>
+    <MediaQuery minWidth={700}>
+      <AppBarDesktop />
+    </MediaQuery>
+  </div>
+);
+export default AppBar;
